9043a4bed7bbc2dabe29702fcaf953120fda26c5
GishaTheDeveloper <spaceshhz@gmail.com>
2020-10-04 20:54:58 +0300

    Add CameraController.cs

---
+++ Town-Generating/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    #region PUBLIC_FIELDS
+    public float rotationSpeed;
+    #endregion
+
+    #region PRIVATE_FIELDS
+    float yRotation;
+    #endregion
+
+    void Start()
+    {
+        yRotation = transform.rotation.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        yRotation += horizontalInput * Time.deltaTime * rotationSpeed;
+
+        transform.rotation = Quaternion.Euler(Vector3.up * yRotation);
+    }
+}
