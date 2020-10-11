a364985999983c73b32e1f8a71cd7c1752bfd5e1
GishaTheDeveloper <spaceshhz@gmail.com>
2020-10-11 12:59:32 +0300

    Add UIManager.cs

---
+++ Town-Generating/Assets/Scripts/Scriptable/ObjectData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Object", menuName = "Scriptable Objects/Object")]
+public class ObjectData : ScriptableObject
+{
+    public string Name;
+    public GameObject Obj;
+    public Sprite Sprite;
+
+    public MeshRenderer MeshRenderer
+    {
+        get
+        {
+            return Obj.GetComponent<MeshRenderer>();
+        }
+    }
+
+    public MeshFilter MeshFilter
+    {
+        get
+        {
+            return Obj.GetComponent<MeshFilter>();
+        }
+    }
+}
